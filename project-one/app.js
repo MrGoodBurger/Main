6f4ed9e246d8b7e9c7c982f61eee0a52c411cfb8
MrGoodBurger <89281700+MrGoodBurger@users.noreply.github.com>
2021-08-26 13:05:24 -0400

    completed question structure

--- project-one/app.js
+++ project-one/app.js
@@ -28,25 +28,100 @@
 //
 
 //Variable
+const question = document.getElementById('question');
+const choice = Array.from(document.getElementsByClassName('choice-text'));
 
-//Functions
-const createQuestionDiv = (charactername) => {
-    let newDiv = document.createElement('div');
-    charactername.appendChild(newDiv)
-    
-}
+let currentQuestion = {};
+let acceptingAnswers = true;
+let score = 0 ;
 
 //Rayya
-const questionOneArray = ['Geldenreich', 'Kingdom of Borsneia', 'Freisen Confederacy', 'Elven Homeland'],
-const questionOne = "What nation does Rayya originate from?"
+let rayyaQuestions = [
+    {
+        question: "What nation does Rayya originate from?",
+        choice1: 'Geldenreich',
+        choice2: 'Kingdom of Borsneia',
+        choice3: 'Freisen Confederacy',
+        choice4: 'Elven Homeland',
+        answer: 3,
+    },
+    {
+        question:"What was Rayya's pet's name?",
+        choice1: 'Creme',
+        choice2: 'Cream',
+        choice3: 'Demon Dog',
+        choice4: "<script file='xxx.js'>",
+        answer: 3,
+    },
+    {
+        question: "What was Rayya's Original Class",
+        choice1:"Shadow Sorcerer",
+        choice2: "Hexblade Warlock",
+        choice3: "Gold Draconic Sorcerer",
+        choice4: "Swashbuckler Rogue",
+        answer: 3,
+    },
+    {
+        question: "Where does Rayya get her power from?",
+        choice1: "Feywild",
+        choice2: "Shadowfell",
+        choice3: "Her Family",
+        choice4: "Who Knows",
+        answer: 2,
+    },
+    {
+        question: "What weapon does Rayya use in melee combat?",
+        choice1: "Glaive",
+        choice2: "Boot Dagger",
+        choice3: "Censer",
+        choice4: "Rapier",
+        answer: 4,
+        
+    },
+    {
+        question: "Who are Rayya's NPCs?",
+        choice1: "Moore and Shurer",
+        choice2: "Moore, Shurer, and 'The Toymaker'",
+        choice3: "Moore, Shurer and Brakovitch",
+        choice4: "Moore, Shurer and 'Mother'",
+        answer: 4,
+        
+    },
+    {
+        question: "What language did Rayya speak with the Xayacoatl fiend in the jungle?",
+        choice1: "Abyssal",
+        choice2: "Infernal",
+        choice3: "Common",
+        choice4: "Elvish",
+        answer: 2,
+        
+    },
+    {
+        question: "How did Rayya die the first time?",
+        choice1: "She and her husband were murdered by dwarven bandits.",
+        choice2: "She was poisoned by a family rival.",
+        choice3: "She doesn't remember.",
+        choice4: "She was sacrificed by her father for eldritch power.",
+        answer: 1,
+        
+    },
+    {
+        question: "What is Rayya's male Elven alias name?",
+        choice1: "Roland Blackspire",
+        choice2: "Ravinder Blackthorn",
+        choice3: "Ramsey Bolton",
+        choice4: "Rupert Bloomdale",
+        answer: 2,
+        
+    },
+    {
+        question: "What is Rayya's favorite stress release?" ,
+        choice1: "Playing the Violin",
+        choice2: "Taking A Bath",
+        choice3: "Plotting Murder",
+        choice4: "Jewler Work",
+        answer: 1,
+        
+    }
+];
 
-const questionTwo = "What was Rayya's pet's name?"
-const questionTwoArray = ['Creme', 'Cream', 'Creme', 'Demon Dog']
-
-const questionThree = "What was Rayya's Original Class"
-const questionThreeArray = ["Shadow Sorcerer", "Hexblade Warlock", "Gold Draconic Sorcerer", "Swashbuckler Rogue"]
-
-const questionFour = "Where does Rayya get her power from?"
-const questionFourArray = ["Feywild", "Shawdowfell", "Her Family", "Who Knows"]
-
-const questionFive = ""
