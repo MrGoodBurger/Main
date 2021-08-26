f18139fdafffd42ab4c9196a4675335c29418f67
MrGoodBurger <89281700+MrGoodBurger@users.noreply.github.com>
2021-08-26 00:52:49 -0400

    restored old code, combined old code with new

--- project-one/app.js
+++ project-one/app.js
@@ -1,66 +1,48 @@
 //Project Name: Shadows of Cherenko Campaign Trivia
 //Project Owner: MrGoodBurger
 
+//Project Requirements:
+//Project uses HTML, CSS, and Javascript for game solution
+//Project uses CSS to style DOM Elements
+//Consistent codeIndentation and naming conventions
+//Game must be functional, allow for at least a single play through, user can win and lose if respective conditions are met.
+//Need at least 30 commits this week.
 
 
 //Story
 //As a User I want to play a web based trivia game
 
 //Requirements
-//Variables
-const 
+//Questions must be text based and neatly separated
+//When clicked, the user can visually see the incorrect and correct answers
+//Game win condition is user get 70% of the question correct
+//Game Has a You Win Message
+//Game is multiple choice
 
+//Extra:
+//There are more than one set of questions, preferablly one per character.
+//Characters have their own containers(cards or collapsable divs??)
 
-
-//Questions to be used in 
-const rayyaQuestions = [{
-    question: "What nation does Rayya hail from?",
-    answers: {
-        a: "Geldenreich",
-        b: "Freisen Conferderacy",
-        c: "Kingdom of Borsneia",
-        d: "Elven Homeland",
-    },
-    correctAnswer: "b",
-},
-{
-    question: "What was Rayya's pet's name?",
-    answers: {
-        a: "Creme",
-        b: "Cream",
-        c: "Demon Dog",
-        d: "Creme",
-    },
-    correctAnswer = "b",
-},
-{
-    question: "What was Rayya's original class?",
-    answers: {
-        a: "Shadow Sorcerer",
-        b: "Hexblade Warlock",
-        c: "Gold Draconic Sorcerer",
-        d: "Swashbuckler Warlock",
-    },
-    correctAnswer = "c",
-},
-]
+//Variable
 
 //Functions
-// const createQuestions = () => {
-//     let createdQuestions = [];
+const createQuestionDiv = (charactername) => {
+    let newDiv = document.createElement('div');
+    charactername.appendChild(newDiv)
+    
+}
 
-//     rayyaQuestions.forEach((currentQuestion, questionNumber) => {
-//             let answersArray = [];
+//Rayya
+const questionOneArray = ['Geldenreich', 'Kingdom of Borsneia', 'Freisen Confederacy', 'Elven Homeland'],
+const questionOne = "What nation does Rayya originate from?"
 
-//             for(let answer in currentQuestion.answersArray){
-//                 answersArray.push(
-//                     `<label>
-//                         <input type ="radio" name="question${questionNumber}"  value="${letter}">
-//                         ${answer} :
-//                         ${currentQuestion.answersArray[answer]}
-//                     </label>`
-//                 );
+const questionTwo = "What was Rayya's pet's name?"
+const questionTwoArray = ['Creme', 'Cream', 'Creme', 'Demon Dog']
 
-//             }
-//     }
-// }
+const questionThree = "What was Rayya's Original Class"
+const questionThreeArray = ["Shadow Sorcerer", "Hexblade Warlock", "Gold Draconic Sorcerer", "Swashbuckler Rogue"]
+
+const questionFour = "Where does Rayya get her power from?"
+const questionFourArray = ["Feywild", "Shawdowfell", "Her Family", "Who Knows"]
+
+const questionFive = ""
