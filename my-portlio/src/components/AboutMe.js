f46927d72796b1d76f344c1cb6fa7c79d52797f5
MrGoodBurger <89281700+MrGoodBurger@users.noreply.github.com>
2021-11-10 12:46:44 -0500

    started portfolio

---
+++ my-portlio/src/components/AboutMe.js
@@ -0,0 +1,18 @@
+import react from "react";
+
+function AboutMe() {
+    return(
+        <div>
+            <div>
+                <img />
+            </div>
+            <div>
+                <h1>About Me</h1>
+                <p>Trent Sanders </p>
+            </div>
+            
+        </div>
+    )
+}
+
+export default AboutMe;
---
+++ my-portlio/src/components/Image.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Image() {
+    return(
+        <div>
+            <img />
+        </div>
+    )
+};
+
+export default Image;
